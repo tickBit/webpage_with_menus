74cb4e0b1e7c8a7bc6476c52c8a7755e64b579dc
tickBit <marjukosk@gmail.com>
2026-02-23 19:00:01 +0200

    First commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+
+  return (
+    <>
+    <header className="App-header">
+        <div className="logo">
+          <p className="logo-text">My Website</p>
+        </div>
+        <div className="menu">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+          <a href="#contact">Contact</a>
+        </div>
+      </header>
+    </>
+    );
+  }
+
+export default Header;
